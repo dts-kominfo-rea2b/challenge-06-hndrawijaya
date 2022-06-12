9df7d9c35a8923206c702d1d3c7d24fcc2dda264
Hendra Wijaya <wijaya.hndra@gmail.com>
2022-06-12 17:42:51 +0700

    Done fnCallBack

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,29 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallBack) => {
+  const resultData = [];
+
+  fs.readFile(file1,{encoding: 'utf-8'}, function spliData(err, data){
+    const fileData = JSON.parse(data);
+    let message = fileData.message;
+    resultData.push(message.split(' ')[1]);
+
+    fs.readFile(file2,{encoding: 'utf-8'}, function splitData(err, data){
+      const fileData = JSON.parse(data);
+      let message = fileData[0].message;
+      resultData.push(message.split(' ')[1]);
+
+      fs.readFile(file3,{encoding: 'utf-8'}, function splitData(err, data){
+        const fileData = JSON.parse(data);
+        let message = fileData[0].data.message;
+        resultData.push(message.split(' ')[1]);
+
+        fnCallBack(err, resultData);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
